fbb0d5285d8ef9d5e36cb2ab193d1b33a2c8af72
JyothiCyril <pjyothi.lakshmi@gmail.com>
2021-02-09 23:01:56 +0530

    Added one new file and modified one existing file

--- src/test/java/com/qa/selenium/testscripts/TC_AmazonSample_002.java
+++ src/test/java/com/qa/selenium/testscripts/TC_AmazonSample_002.java
@@ -1,1 +1,4 @@
 // This is just a sample file used for demonstration
+
+
+// Adding new lines to the existing file
---
+++ src/test/java/com/qa/selenium/testscripts/TC_AmazonSignIn_003.java
@@ -0,0 +1,8 @@
+package com.qa.selenium.testscripts;
+
+public class TC_AmazonSignIn_003 {
+	
+	
+	// Sample Demonstration
+
+}
