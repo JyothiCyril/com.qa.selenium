807c5518f1fcf989da4123c12ba3956e5c83ed69
JyothiCyril <pjyothi.lakshmi@gmail.com>
2021-09-03 16:39:57 +0530

    Added the code

--- src/test/java/com/qa/selenium/testscripts/TC_AmazonTest_004.java
+++ src/test/java/com/qa/selenium/testscripts/TC_AmazonTest_004.java
@@ -3,7 +3,7 @@ package com.qa.selenium.testscripts;
 public class TC_AmazonTest_004 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("New code added");
 
 	}
 
