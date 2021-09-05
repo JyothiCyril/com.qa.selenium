53e5758d38644d36986e1ac01434441d96621f57
Rashi Singh <srashi801@gmail.com>
2021-09-05 15:50:38 +0530

    QA rashi added new demo class

---
+++ src/test/java/com/qa/selenium/testscripts/Demo.java
@@ -0,0 +1,9 @@
+package com.qa.selenium.testscripts;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("creating new branch and pushing and commiting using that branch");
+		
+	}
+
+}
